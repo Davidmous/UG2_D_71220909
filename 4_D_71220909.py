6cfe6bcd6c2b03e035ee26c7d8977e2e27e931a2
Davidmous <117893895+Davidmous@users.noreply.github.com>
2023-02-15 19:30:32 +0700

    Update 4_D_71220909.py

--- 4_D_71220909.py
+++ 4_D_71220909.py
@@ -1,1 +1,22 @@
 print('','','!! Selamat datang di H+ GYM !!')
+print('Silahkan pilih menu dibawah ini:')
+print('1. Menambah data')
+print('2. Menampilkan data')
+print('3. Keluar')
+pilihan = input('Masukan Pilihan yang anda inginkan : ')
+
+data_nama = []
+data_member = []
+
+for i in pilihan :
+    if pilihan == '1' :
+        nama = str(input('Masukan nama pelanggan : '))
+        member = str(input('Masukkan jenis member : '))
+        data_member.insert(0,nama)
+        data_member.insert(0,member)
+        print('Data sudah berhasil ditambahkan!')
+    elif pilihan == '2':
+        print(data_nama)
+        print(data_member)
+    else :
+        break
